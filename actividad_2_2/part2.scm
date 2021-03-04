fce0c294a857f7017d14972559f29b66dc4805fb
ACMR1009 <46897998+ACMR1009@users.noreply.github.com>
2021-03-04 10:14:33 -0700

    Update part2.scm

--- actividad_2_2/part2.scm
+++ actividad_2_2/part2.scm
@@ -3,7 +3,7 @@
 ;; 1. insert
 (define (insert n lst)
     (cond
-        ((null? lst) list n)
+        ((null? lst) (list n))
         ((< n (car lst)) (cons n lst))
         (else (cons (car lst) (insert n (cdr lst))))
     )
