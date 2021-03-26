2fb80efe86cbf7dfaac2fa7f5492e4ff569dbcbc
oscar-me <oscar-me@outlook.com>
2021-03-25 21:10:41 -0600

    add RuleSet class

--- actividad_3_2/DFA.cpp
+++ actividad_3_2/DFA.cpp
@@ -39,6 +39,61 @@ public:
 };
 
 
+class RuleSet {
+private:
+    unordered_map<char, vector<Rule>> rules;
+public:
+    RuleSet() {}
+
+    void loadFromFile(string path) {
+        ifstream file;
+        string line, cell, print;
+        stringstream ss;
+        vector<char> chars;
+        
+        file.open(path);
+        if (file.fail()) {
+            throw runtime_error("Cannot read file");
+        }
+
+        // leer encabezado (caracteres)
+        getline(file, line);
+        getline(file, line);
+        ss << line;
+        while (getline(ss, cell, ',')) {
+            chars.push_back(cell[0]);
+        }
+        ss.str(string());
+        ss.clear();
+
+        // leer cuerpo de tabla (reglas)
+        int stateNum = 0;
+        while (getline(file, line)) {
+            ss << line;
+            for (int i = 0; i < chars.size(); i++) {
+                getline(ss, cell, ',');
+                this->rules[chars[i]].push_back(Rule(chars[i], stateNum, stoi(cell) ));
+            }
+
+            stateNum++;
+
+            ss.str(string());
+            ss.clear();
+        }
+
+        file.close();
+    }
+        
+    Rule getRule(char character, int currentState) {
+        if (this->rules.find(character) != rules.end()) {
+            // si la key existe
+            return this->rules[character][currentState];
+        } else {
+            // si no existe, devolver regla error
+            return Rule(character, currentState, 17);
+        }
+    }
+};
 
 
 int main() {
