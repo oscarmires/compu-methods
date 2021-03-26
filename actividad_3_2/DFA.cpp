5e6bd828fe3a7aaad44c52dcfdcfd4f52d10e4ad
ACMR1009 <46897998+ACMR1009@users.noreply.github.com>
2021-03-25 20:17:47 -0700

    Create DFA.cpp
    Added class Automata and updated main()

--- actividad_3_2/DFA.cpp
+++ actividad_3_2/DFA.cpp
@@ -38,65 +38,129 @@ public:
     }
 };
 
-
-class RuleSet {
+class Automata {
 private:
-    unordered_map<char, vector<Rule>> rules;
+    RuleSet rules;
+    int currentState;
+    string stateNames[19] {
+        "",
+        "entero", 
+        "real", 
+        "real", 
+        "real", 
+        "real", 
+        "real", 
+        "variable",
+        "asignación",
+        "suma",
+        "resta",
+        "multiplicación",
+        "división",
+        "potencia",
+        "comentario",
+        "abre paréntesis",
+        "cierra paréntesis",
+        "error",
+        ""
+    };
 public:
-    RuleSet() {}
-
-    void loadFromFile(string path) {
-        ifstream file;
-        string line, cell, print;
-        stringstream ss;
-        vector<char> chars;
-        
-        file.open(path);
-        if (file.fail()) {
-            throw runtime_error("Cannot read file");
-        }
-
-        // leer encabezado (caracteres)
-        getline(file, line);
-        getline(file, line);
-        ss << line;
-        while (getline(ss, cell, ',')) {
-            chars.push_back(cell[0]);
-        }
-        ss.str(string());
-        ss.clear();
-
-        // leer cuerpo de tabla (reglas)
-        int stateNum = 0;
-        while (getline(file, line)) {
-            ss << line;
-            for (int i = 0; i < chars.size(); i++) {
-                getline(ss, cell, ',');
-                this->rules[chars[i]].push_back(Rule(chars[i], stateNum, stoi(cell) ));
-            }
-
-            stateNum++;
-
-            ss.str(string());
-            ss.clear();
-        }
-
-        file.close();
+    Automata() {
+        this->currentState = 0;
     }
-        
-    Rule getRule(char character, int currentState) {
-        if (this->rules.find(character) != rules.end()) {
-            // si la key existe
-            return this->rules[character][currentState];
-        } else {
-            // si no existe, devolver regla error
-            return Rule(character, currentState, 17);
+
+    void loadRules(string path) {
+        try {
+            this->rules.loadFromFile(path);
+        } catch(const runtime_error& re) {
+            throw re;
         }
+    }
+
+    void AnalisisCaracteres(string dato)
+    {
+        Rule r1;
+        string token = "";
+        for (int i = 0; i < dato.length(); i++) {	
+            r1 = rules.getRule(dato[i], currentState);
+            if (r1.getNext() == 18) { // si llega a final del token
+                cout << token << "\t\t\t\t" << this->stateNames[currentState] << endl;
+                token = "";
+                restart();
+                r1 = rules.getRule(dato[i], currentState);
+            }
+            this->currentState = r1.getNext();
+            if (currentState != 0) {  // si ya comenzó token (esto para ignorar espacios previos)
+                token += dato[i];
+            }
+        }
+        
+        // si terminó como un punto aislado, marcar como error
+        if (currentState == 2 && token == ".") currentState = 17;
+
+        // imprimir último token
+        cout << token << "\t\t\t\t" << this->stateNames[currentState] << endl;
+        token = "";
+        restart();
+    }
+    
+    void lexerAritmetico(string archivo) {
+        // leer un código
+
+        ifstream ficheroEntrada;
+        string dato;
+
+        ficheroEntrada.open(archivo, ios::in);
+        if (ficheroEntrada.is_open()) {
+            while (!ficheroEntrada.eof())
+            {
+
+                stringstream ss(dato);
+
+                while (getline(ficheroEntrada, dato))
+                {
+                        //cout << "nuevo ciclo" << endl;
+                        AnalisisCaracteres(dato.c_str());
+
+                }
+                cout << endl;
+            }
+            ficheroEntrada.close();
+        }
+        else cout << "Fichero inexistente" << endl;
+    }
+
+    // Función para probar tokens individuales
+    void readSingleToken(string token) {
+        
+        token += ' ';
+        Rule currentRule;
+        for (char c : token) {
+            currentRule = rules.getRule(c, this->currentState);
+            if (currentRule.getNext() == 17) {
+                cout << token << "\t\terror" << endl;
+                this->restart();
+                return;
+            } else if (currentRule.getNext() == 18) {
+                cout << token << "\t\t" << stateNames[currentState] << endl;
+                this->restart();
+                return;
+            } else {
+                this->currentState = currentRule.getNext();
+            }
+        }
+        cout << token << "\t\t" << stateNames[currentState] << endl;
+        this->restart();
+    }
+
+    void restart() {
+        this->currentState = 0;
     }
 };
 
-
 int main() {
 
+    Automata automata;
+    automata.loadRules("DFAtable.csv");
+    automata.lexerAritmetico("TextFile1.txt");
     return 0;
 }
