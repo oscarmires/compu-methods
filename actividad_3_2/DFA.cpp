04162a6843030d45c43aaa604cfbd988db5fde80
ariclavel <a01706629@itesm.mx>
2021-03-25 21:34:58 -0400

    codigo clase Rule

--- actividad_3_2/DFA.cpp
+++ actividad_3_2/DFA.cpp
@@ -6,6 +6,41 @@
 #include <unordered_map>
 using namespace std;
 
+
+class Rule {
+private:
+    char character;
+    int state, 
+        next;
+public:
+    Rule() {}
+
+    Rule(char character, int state, int next) {
+        this->character = character;
+        this->state = state;
+        this->next = next;
+    }
+
+    char getCharacter() {
+        return this->character;
+    }
+
+    int getState() {
+        return this->state;
+    }
+
+    int getNext() {
+        return this->next;
+    }
+
+    string toString() {
+        return "State: " + to_string(this->state) + ", Char: '" + this->character + "' -> " + to_string(this->next);;
+    }
+};
+
+
+
+
 int main() {
 
     return 0;
