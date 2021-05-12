68009ffca0631dd47438f7faff06f0e2555ad7b0
oscar-me <oscar-me@outlook.com>
2021-05-12 18:52:47 -0500

    add 3 ex to act_5_1

--- actividad_5_1/act_5_1.pl
+++ actividad_5_1/act_5_1.pl
@@ -13,13 +13,27 @@
 
 
 % 7
-
+fact(0, 1).
+fact(N, F):-
+    N > 0,
+    N_1 is N - 1,
+    fact(N_1, F_1),
+    F is F_1 * N.
 
 % 9 
-
+length2([], 0).
+length2([_|T], L):-
+    length2(T, L_1),
+    L is L_1 + 1.
 
 % 10
 
 
 % 16
-
+sum([], _):-
+    fail.
+sum([H], S):-
+    S is H.
+sum([H|T], S):-
+    sum(T, S_1),
+    S is H + S_1.
